b90ef67cfe5e67861f78a7aa589a33cf0a1368dd
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-21 00:57:07 +0500

    sidebars-completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "John Doe" };
+  const loggedIn = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "contact@johndoe.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 501.6 }]}
+      />
     </section>
   );
 };
