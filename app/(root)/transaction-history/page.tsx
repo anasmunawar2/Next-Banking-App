5e95a6db0cdf1ad07abbed91f33d24d6801117cc
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-21 22:03:21 +0500

    update

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TransactionHistory = () => {
-  return <div>TransactionHistory</div>;
+  return <div>Transaction History</div>;
 };
 
 export default TransactionHistory;
