ca0ec4fc2f061da86bbad7caf1e3c887df65f065
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-24 02:22:30 +0500

    auth-completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,8 +18,10 @@ import {
 import { authFormSchema } from "@/lib/utils";
 import CustomInput from "./CustomInput";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -33,11 +35,42 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
-  }
+
+    try {
+      // Sign up with Appwrite & create plaid token
+
+      if (type === "sign-up") {
+        // const userData = {
+        //   firstName: data.firstName!,
+        //   lastName: data.lastName!,
+        //   address1: data.address1!,
+        //   city: data.city!,
+        //   state: data.state!,
+        //   postalCode: data.postalCode!,
+        //   dateOfBirth: data.dateOfBirth!,
+        //   ssn: data.ssn!,
+        //   email: data.email,
+        //   password: data.password,
+        // };
+        // const newUser = await signUp(userData);
+        // setUser(newUser);
+      }
+
+      if (type === "sign-in") {
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // });
+        // if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8">
