553a81a07e213f7fb0879273e69d676b64658579
anasmunawar2 <anasmunawar2@gmail.com>
2024-06-04 22:39:36 +0500

    transactions

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,29 +1,33 @@
 "use client";
-import React from "react";
+
 import {
   Sheet,
   SheetClose,
   SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import Image from "next/image";
-import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
-  const pathName = usePathname();
+  const pathname = usePathname();
+
   return (
-    <section className="w-full max-w-[264px]">
+    <section className="w-fulll max-w-[264px]">
       <Sheet>
         <SheetTrigger>
           <Image
-            alt="menu"
             src="/icons/hamburger.svg"
             width={30}
             height={30}
+            alt="menu"
             className="cursor-pointer"
           />
         </SheetTrigger>
@@ -32,38 +36,42 @@ const MobileNav = ({ user }: MobileNavProps) => {
             href="/"
             className="cursor-pointer flex items-center gap-1 px-4"
           >
-            <Image src="/icons/logo.svg" width={34} height={34} alt="logo" />
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
-              Banking App
+              Horizon
             </h1>
           </Link>
-
           <div className="mobilenav-sheet">
             <SheetClose asChild>
               <nav className="flex h-full flex-col gap-6 pt-16 text-white">
                 {sidebarLinks.map((item) => {
                   const isActive =
-                    pathName === item.route ||
-                    pathName.startsWith(`${item.route}/`);
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+
                   return (
                     <SheetClose asChild key={item.route}>
                       <Link
+                        href={item.route}
+                        key={item.label}
                         className={cn("mobilenav-sheet_close w-full", {
                           "bg-bank-gradient": isActive,
                         })}
-                        href={item.route}
-                        key={item.label}
                       >
                         <Image
-                          alt={item.label}
                           src={item.imgURL}
+                          alt={item.label}
                           width={20}
                           height={20}
                           className={cn({
                             "brightness-[3] invert-0": isActive,
                           })}
                         />
-
                         <p
                           className={cn("text-16 font-semibold text-black-2", {
                             "text-white": isActive,
@@ -78,6 +86,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 USER
               </nav>
             </SheetClose>
+
             <Footer user={user} type="mobile" />
           </div>
         </SheetContent>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,16 @@
 "use client";
+
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const Sidebar = ({ user }: SiderbarProps) => {
-  const pathName = usePathname();
+  const pathname = usePathname();
+
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -17,39 +19,42 @@ const Sidebar = ({ user }: SiderbarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="logo"
+            alt="Horizon logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Banking App</h1>
+          <h1 className="sidebar-logo">Horizon</h1>
         </Link>
+
         {sidebarLinks.map((item) => {
           const isActive =
-            pathName === item.route || pathName.startsWith(`${item.route}/`);
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
             <Link
-              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
               href={item.route}
               key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
             >
               <div className="relative size-6">
                 <Image
-                  alt={item.label}
                   src={item.imgURL}
+                  alt={item.label}
                   fill
                   className={cn({
                     "brightness-[3] invert-0": isActive,
                   })}
                 />
               </div>
-
               <p className={cn("sidebar-label", { "!text-white": isActive })}>
                 {item.label}
               </p>
             </Link>
           );
         })}
-        USER
+
+        <PlaidLink user={user} />
       </nav>
+
       <Footer user={user} />
     </section>
   );
